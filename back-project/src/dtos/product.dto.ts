529c72b45dac931e5cb61b9f59dbc4588ea97831
GonzaG1501 <123435567+GonzaG1501@users.noreply.github.com>
2024-05-28 18:45:08 -0300

    progreso de module user

---
+++ back-project/src/dtos/product.dto.ts
@@ -0,0 +1,9 @@
+export class ProductDto {
+  name;
+
+  description;
+
+  price;
+
+  category;
+}
--- back-project/src/modules/product/product.controller.ts
+++ back-project/src/modules/product/product.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  ParseUUIDPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductDto } from 'src/dtos/product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productsService: ProductService) {}
+
+  @Get()
+  getAllProducts() {
+    this.productsService.getAllProducts();
+  }
+
+  @Post()
+  createProduct(product: ProductDto) {
+    this.productsService.createProduct(product);
+  }
+
+  @Put()
+  updateProduct(product: ProductDto) {
+    this.productsService.updateProduct(product);
+  }
+
+  @Delete()
+  deleteProduct(id: ParseUUIDPipe) {
+    this.productsService.deleteProduct(id);
+  }
+}

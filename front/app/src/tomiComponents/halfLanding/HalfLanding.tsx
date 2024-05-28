0968ef61547ad34f44e545dfc554073a572b1489
tomas.avila <tomysykess@gmail.com>
2024-05-28 20:34:06 -0300

    tomiComponent

--- front/app/src/tomiComponents/halfLanding/HalfLanding.tsx
+++ front/app/src/tomiComponents/halfLanding/HalfLanding.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Link from "next/link";
+
 const HalfLanding = () => {
   return (
     <div>
       component HalfLanding
       <div>
-        <img src="" alt="halfImage" />
-        <Link href={"/home"}>
-          <div>elButton</div>
-        </Link>
+        <img
+          src="/liquors1.jpg"
+          alt="halfImage"
+          className="max-w-full max-h-auto"
+        />
+
+        <button className="flex items-center justify-center bg-white text-black m-2 p-2 w-20 border border-black rounded">
+          elButton <Link href="/home"></Link>
+        </button>
       </div>
     </div>
   );

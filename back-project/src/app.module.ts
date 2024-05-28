9cfa55776f401ce5a26f4e0d99197a07c156c287
Dantalian1840 <104804632+Dantalian1840@users.noreply.github.com>
2024-05-28 17:46:02 -0500

    Reviews CRUD + Entity

--- back-project/src/app.module.ts
+++ back-project/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReviewsModule } from './modules/reviews/reviews.module';
 
 @Module({
-  imports: [],
+  imports: [ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })

ed8149dc9207f0b328d72c45a4e94a590182752e
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-05-29 17:32:59 +0200

    reparacion de la carpeta componente
    cambiamos el nombre de la carpeta del tomi a components y la seteamos correctamente, reparando la landing

--- front/next-project/src/app/page.tsx
+++ front/next-project/src/app/page.tsx
@@ -1,8 +1,8 @@
-import SquareWineLanding from "@/tomiComponents/squareLanding/SquareWineLanding";
-import HalfLanding from "@/tomiComponents/halfLanding/HalfLanding";
-import SquareWhiskeyLanding from "@/tomiComponents/squareLanding/SquareWhiskeyLanding";
-import SquareSpiritsLanding from "@/tomiComponents/squareLanding/SquareSpiritsLanding";
-import SquareRumLanding from "@/tomiComponents/squareLanding/SquareRumLanding";
+import SquareWineLanding from "@/components/squareLanding/SquareWineLanding";
+import HalfLanding from "@/components/halfLanding/HalfLanding";
+import SquareWhiskeyLanding from "@/components/squareLanding/SquareWhiskeyLanding";
+import SquareSpiritsLanding from "@/components/squareLanding/SquareSpiritsLanding";
+import SquareRumLanding from "@/components/squareLanding/SquareRumLanding";
 export default function Landing() {
   return (
     <div className="flex flex-row">

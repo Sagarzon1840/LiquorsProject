619aef2ffbaa00ad31d21efb9b726b1fb81a0739
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-05-29 18:04:05 -0300

    -renderizo en home prueba de contenedor de productos

--- front/next-project/src/app/home/page.tsx
+++ front/next-project/src/app/home/page.tsx
@@ -1,5 +1,12 @@
-const Home = () => {
-  return <div>page Home</div>;
+import React from "react";
+import BannerProducts from "@/components/bannerProductHome/banner";
+
+const Home: React.FC = ():React.ReactNode => {
+  return (
+    <>
+     <BannerProducts/>
+    </>
+  )
 };
 
 export default Home;

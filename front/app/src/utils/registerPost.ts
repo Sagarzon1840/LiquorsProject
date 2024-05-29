b004fe0f8549b1cccee49abae4cf3f1072dd1a27
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-05-29 02:28:15 -0300

    -creacion de rutas: login y register -creacion de componentes login y register en dichas rutas. -creacion de carpeta utils:  *Funcion para validar el formulario registro (aun no utilizada)  *Funciones async para el POST a la API del formulario Register y el Login  *interfaces de datos.

---
+++ front/app/src/utils/registerPost.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const registerUser = async (formData: any, setIsSuccess: any, setErrors: any, router: any, errors: any, setIsLoading: any) => {
+  try {
+        const response = await axios.post('http://localhost:3002/users/register', formData);
+        setIsSuccess(true);
+        setErrors({});
+        setTimeout(() => {
+        alert("redirigiendo a login")
+        router.push("/login")
+        }, 3000)
+  } catch (error: any) {
+        setErrors({ ...errors, submit: `Error al registrar al usuario: ${error.response.data.message}` });
+  } finally {
+        setIsLoading(false);
+  }
+};
+
+export default registerUser;
--- front/app/tailwind.config.ts
+++ front/app/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        wine: "#c23a2e",
+      },
+      fontFamily: {
+        'Lato': ['Lato', 'sans-serif'],
+      },
     },
   },
   plugins: [],

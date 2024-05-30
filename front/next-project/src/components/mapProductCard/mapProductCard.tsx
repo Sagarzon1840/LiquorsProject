cf497f008fb7e18313a377ccc3d842c8262a69d0
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-05-30 18:25:52 -0300

    -agrego un contenedor flex con propiedad al componente que mapea la card, para evitar que las cards sobrepasen el ancho de la pag.

--- front/next-project/src/components/mapProductCard/mapProductCard.tsx
+++ front/next-project/src/components/mapProductCard/mapProductCard.tsx
@@ -22,9 +22,11 @@ export const MapProductCard: React.FC = (): React.ReactNode => {
   return (
       <>
           {/*Mapea el arreglo de productos y renderiza un Card para cada uno*/}
-          {arrayWines.map((product) => (
+          <div className="flex space-x-4">
+            {arrayWines.map((product:any) => (
               <ProductCard key={product.id} product={product} />
-          ))}
+            ))}
+          </div>
       </>
   );
 };

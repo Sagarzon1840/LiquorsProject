a452bc82c2d0c0439b8bc6ef0639a221fb0af88b
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-05-30 18:24:01 -0300

    -renderizo las tarjetas de products en la ruta Product *aun falta darle estilos al contenedor de todas las tarjetas.

--- front/next-project/src/app/product/page.tsx
+++ front/next-project/src/app/product/page.tsx
@@ -1,5 +1,14 @@
-const Product = () => {
-  return <div>page Product</div>;
+import BannerProducts from "@/components/bannerProductHome/banner";
+
+const Product: React.FC = ():React.ReactNode => {
+  return (
+    <div className="flex flex-col overflow-auto pt-10 items-center">
+      <h1 className="text-center text-2xl font-bold mb-4">
+        Explora nuestras categorías de vinos
+      </h1>
+      <BannerProducts />
+  </div>
+  )
 };
 
 export default Product;

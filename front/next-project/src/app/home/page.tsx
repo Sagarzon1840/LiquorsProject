a99e66e5dd61e3bbb5b1ec58574c4fb4305e9a95
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-05-30 18:23:02 -0300

    -quito el componente que tiene las tarjetas de products de la ruta home, ya que ahi tomas renderizara su lading

--- front/next-project/src/app/home/page.tsx
+++ front/next-project/src/app/home/page.tsx
@@ -1,12 +1,8 @@
 import React from "react";
-import BannerProducts from "@/components/bannerProductHome/banner";
 
 const Home: React.FC = ():React.ReactNode => {
   return (
     <>
-      <BannerProducts/>
-      <hr></hr>
-      <BannerProducts/>
     </>
   )
 };

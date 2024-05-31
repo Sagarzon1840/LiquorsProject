4b63c52004e8b4c600d2877a023e99437338acbc
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-05-31 02:45:45 +0200

    creacion de la page productDetail
    se crearon los componentes de productDetail, review, reviewContainer y reviewForm.
    ademas incorpore estilos en el global.

--- front/next-project/src/app/product/page.tsx
+++ front/next-project/src/app/product/page.tsx
@@ -1,5 +1,22 @@
-const Product = () => {
-  return <div>page Product</div>;
+import { ProductDetail } from "@/components/productDetail/productDetail";
+
+const productPreload = {
+  id: 1,
+  name: "nombre Vino",
+  description: "Descripcion del vino",
+  category: "vino",
+  image: "/vinoCard.jpg",
+  price: 100
+  }
+
+const Product = async ({params}: {params: { productId: string }}) => {
+  // const product = await getDataById(String(params.productId))
+
+  return (
+    <ProductDetail product={productPreload} />
+  )
+
+
 };
 
 export default Product;

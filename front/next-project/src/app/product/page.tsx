3e87ecdf0b62a5ace6785826169831383d26dd2c
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-05-31 00:06:14 -0300

    -presentando mas tarjetas

--- front/next-project/src/app/product/page.tsx
+++ front/next-project/src/app/product/page.tsx
@@ -3,10 +3,23 @@ import BannerProducts from "@/components/bannerProductHome/banner";
 const Product: React.FC = ():React.ReactNode => {
   return (
     <div className="flex flex-col overflow-auto pt-10 items-center">
-      <h1 className="text-center text-2xl font-bold mb-4">
+
+      {/*SECCION UNO DE TARJETAS */}
+      <h1 className="text-center text-2xl text-white font-bold mb-4">
         Explora nuestras categorías de vinos
       </h1>
-      <BannerProducts />
+      <div className="flex space-x-4">
+        <BannerProducts />
+      </div>
+
+      {/*SECCION DOS DE TARJETAS */}
+      <h1 className="text-center text-2xl mt-6 text-white font-bold mb-4">
+        Explora nuestras categorías de vinos
+      </h1>
+      <div className="flex space-x-4">
+        <BannerProducts />
+      </div>
+
   </div>
   )
 };

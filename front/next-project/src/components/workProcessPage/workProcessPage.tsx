1f23c01ab3cfd7133412145e0b207a8e43199d76
Cesar Zeballos <126284948+CesarZeballos@users.noreply.github.com>
2024-06-01 20:02:57 +0200

    workInProcess page
    creacion del componente work in process page y llamado al componente en la pagina de producto recomendado

---
+++ front/next-project/src/components/workProcessPage/workProcessPage.tsx
@@ -0,0 +1,6 @@
+export const WorkProcessPage = () => {
+    return <div className="flex flex-col justify-center items-center my-10">
+        <img src="/work in process.png" alt="workProcess" />
+        <h1 className="header1">Estamos trabajando para crear esta pagina</h1>
+    </div>;
+};

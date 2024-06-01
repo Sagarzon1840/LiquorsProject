46d728a53cba1187ab587cb4c512a33b0d6d1331
joelch97 <125751366+Wirreles@users.noreply.github.com>
2024-06-01 18:32:49 -0300

    traigoYllevo

--- back-project/src/dtos/updateSubscription.dto.ts
+++ back-project/src/dtos/updateSubscription.dto.ts
@@ -1,9 +1,18 @@
 import { IsString } from 'class-validator';
 
 export class UpdateSubscriptionDto {
+
+  /**
+   * Es un tipo de subcripcion para el usuario
+   * @example seller
+   */
   @IsString()
   type: string;
 
+  /**
+   * Status de la subscripcion
+   * @example active
+   */
   @IsString()
   status: string;
 }

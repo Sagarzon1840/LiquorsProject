1b18ed38ec4b371106b1628122080d74bb93bd3d
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-02 03:00:21 -0300

    -mejore el scroll de las tarjetas del home, se desplaza corte carrussel. harcodie colores de borde para simular como se veria segun categoria del banner.

--- front/next-project/src/components/bannerProductHome/banner.tsx
+++ front/next-project/src/components/bannerProductHome/banner.tsx
@@ -1,64 +1,70 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import MapProductCard from "../mapProductCard/mapProductCard";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { Product } from "@/interfaces/interfaz";
+import 'tailwindcss/tailwind.css';
+import { useSelector } from "react-redux";
 
 const BannerProducts: React.FC = () => {
-
   const scrollContainer = useRef<HTMLDivElement>(null);
+  const [currentSection, setCurrentSection] = useState(0);
+  const cardsPerPage = 3;
+  const dataGlobal = useSelector((state: any) => state.products.data);
 
-  //Función para hacer scroll hacia la izquierda con animación
-  const scrollLeft = () => {
+  // Calcula el número máximo de secciones basado en la cantidad de tarjetas y tarjetas por página.
+  const maxSections = Math.ceil(dataGlobal.length / cardsPerPage) - 1;
+
+  const scroll = (direction: "left" | "right") => {
     if (scrollContainer.current) {
-      scrollContainer.current.classList.add("slide-left");
-      scrollContainer.current.scrollLeft -= 200;
-      // Eliminar la clase de animación después de que termine la animación
-      scrollContainer.current.addEventListener(
-        "animationend",
-        () => {
-          scrollContainer.current?.classList.remove("slide-left");
-        },
-        { once: true }
-      ); // El evento se escuchará solo una vez
+      if (direction === "left" && currentSection > 0) {
+        setCurrentSection(currentSection - 1);
+      } else if (direction === "right" && currentSection < maxSections) {
+        setCurrentSection(currentSection + 1);
+      }
     }
   };
 
-  //Función para hacer scroll hacia la derecha con animación
-  const scrollRight = () => {
+  // UseEffect para ajustar la posición del scroll del contenedor de tarjetas
+  useEffect(() => {
     if (scrollContainer.current) {
-      scrollContainer.current.classList.add("slide-right");
-      scrollContainer.current.scrollLeft += 200;
-      // Eliminar la clase de animación después de que termine la animación
-      scrollContainer.current.addEventListener(
-        "animationend",
-        () => {
-          scrollContainer.current?.classList.remove("slide-right");
-        },
-        { once: true }
-      ); // El evento se escuchará solo una vez
+      const scrollAmount = scrollContainer.current.offsetWidth;
+      scrollContainer.current.scrollTo({
+        left: currentSection * scrollAmount,
+        behavior: 'smooth',
+      });
     }
-  };
+  }, [currentSection]);
 
   return (
-    <div className="relative flex justify-center flex-col pt-7 pb-4 bg-grey5 w-3/4 max-w-screen-lg mx-auto">
+    <div className="relative flex justify-center flex-col pt-7 pb-4 bg-greyMLfilter w-3/4 max-w-screen-lg mx-auto">
       <div className="flex justify-center items-center relative w-full">
         {/* FLECHA IZQUIERDA */}
-        <button onClick={scrollLeft} className="absolute left-0 p-2 z-10">
-          <ArrowBackIosIcon className="h-6 w-6 text-gray-700" />
+        <button 
+          onClick={() => scroll('left')} 
+          className="absolute left-0  mr-96 p-2 z-10" 
+          disabled={currentSection === 0}
+        >
+          <div className="rounded-full bg-wine p-2 ">
+            <ArrowBackIosIcon className="h-6 w-6 text-white" />
+          </div>
         </button>
-
         {/* Contenedor de tarjetas */}
         <div ref={scrollContainer} className="flex overflow-hidden w-full">
-          <div className="flex space-x-4">
+          <div className="flex space-x-4 transition-transform duration-500">
             <MapProductCard />
           </div>
         </div>
 
         {/* FLECHA DERECHA */}
-        <button onClick={scrollRight} className="absolute right-0 p-2 z-10">
-          <ArrowForwardIosIcon className="h-6 w-6 text-gray-700" />
+        <button 
+          onClick={() => scroll('right')} 
+          className="absolute right-0 p-2 z-10" 
+          disabled={currentSection === maxSections}
+        >
+          <div className="rounded-full bg-wine p-2 ">
+            <ArrowForwardIosIcon className="h-6 w-6 text-white" />
+          </div>
         </button>
       </div>
       <section className="flex justify-start text-white mt-4">

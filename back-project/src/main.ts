92cf6697caf2587c0eef21cc659a4dd6b164e951
joelch97 <125751366+Wirreles@users.noreply.github.com>
2024-06-01 21:04:37 -0300

    con cors

--- back-project/src/main.ts
+++ back-project/src/main.ts
@@ -4,7 +4,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.enableCors({
+    origin: 'http://localhost:3000', //Permitir solo solicitudes desde este origen
+  });
   const swaggerConfig = new DocumentBuilder()
   .setTitle('Documentacion del backend de Liqours')
   .setDescription('Rutas para el uso del servidor')

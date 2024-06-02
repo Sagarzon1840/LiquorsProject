a1cfaad749a65c126cdb214cd67dca53414b382e
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-02 02:49:19 -0300

    -agregue la fuente que use

--- front/next-project/src/app/layout.tsx
+++ front/next-project/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "@/components/navbar/navbar";
-
 import Providers from "@/store/providers";
 import { Footer } from "@/components/footer/footer";
 
@@ -23,7 +22,12 @@ export default function RootLayout({
       <body className={inter.className}>
         <Providers>
           <NavBar />
-
+          <hr></hr>
+          {/* fuente plus-jakarta */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
+           {/* ----------- */}
           {children}
         </Providers>
       </body>

6334ec9dfa7093051715a517b9bc9c4509bfb047
tomas.avila <tomysykess@gmail.com>
2024-06-03 10:39:14 -0300

    baners

--- front/next-project/src/utils/arrayRecommendations.ts
+++ front/next-project/src/utils/arrayRecommendations.ts
@@ -1,7 +1,7 @@
 export const arrayRecommendations = [
   {
     title: "Lo Más Comprado",
-    image: "/margarita.png",
+    image: "/month1.png",
     description:
       "La clásica Margarita, una bebida que nunca pasa de moda. Perfecta para cualquier celebración.",
     color: "beige",
@@ -9,7 +9,7 @@ export const arrayRecommendations = [
   },
   {
     title: "Más Elegante",
-    image: "/negroni.png",
+    image: "/month2.png",
     description:
       "El Negroni es la elección perfecta para una noche sofisticada. Un cóctel con carácter y estilo.",
     color: "#800000",
@@ -17,7 +17,7 @@ export const arrayRecommendations = [
   },
   {
     title: "Para los Amantes del Whisky",
-    image: "/whis.png",
+    image: "/month3.png",
     description:
       "El Old Fashioned es el cóctel perfecto para los amantes del whisky. Sencillo, pero lleno de sabor.",
     color: "#8B4513",

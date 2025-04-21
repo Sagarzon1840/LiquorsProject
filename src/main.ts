3077c81c0469bb1464c0d20c9d0e4577cb8ceec4
Juan Pablo Del Rosso <139802501+JuanPaBL2@users.noreply.github.com>
2025-04-21 15:07:09 -0300

    Update PORT.ts
    Agrego enviroment PORT para que render asigne puerto en despliegue

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
